a9064b771adfcd6e576f1cac083c76577eb12508
Gabriel Nathan Santos Frutuoso <2401693@SALAS.AULAS>
2025-04-14 19:53:00 -0300

    FEAT: primeira adição de arquivos bases

---
+++ Assets/CharCreation/points.py
@@ -0,0 +1,11 @@
+from Assets.funcs import wait
+
+def distribuir(name):
+    while True:
+        print(f"Bem-Vindo {name} a distribuição de pontos do Randventure!")
+        wait()
+        print(f"Iremos fazer a distribuição de pontos do seu personagem")
+        wait()
+        print(f"Quer saber quais são os melhores tipo de distribuição para o seu personagem?")
+        
+        
---
+++ main.py
@@ -0,0 +1,4 @@
+from time import sleep as t
+
+def wait():
+    t(1)
